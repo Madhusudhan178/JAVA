4d9d43f2ebcdc2f41fc37d36afe709bf5deb5811
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-05-12 17:28:21 +0530

    initial commit

--- JAVA-1/prgm2.java
+++ JAVA-1/prgm2.java
@@ -1,6 +1,8 @@
 public class prgm2 {
      public static void main(String[] args) {
           System.out.println("Hi");
+          int a=7;
+          System.out.println(a++ +" Dhoni");
      }
      
 }
