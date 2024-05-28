0a448786a915adc3a1e9366cf903c7e22a60bbd6
Madhusudhan178 <madhusudhanmayur@gmail.com>
2024-05-28 13:07:42 +0530

    initial commit

---
+++ JAVA-1/prgm1.java
@@ -0,0 +1,15 @@
+class hello {
+     public static void main(String[] args)
+     {
+       System.out.print("Hello");
+     }  
+     
+};
+public class prgm1{
+  public static void main(String... args)
+     {
+       System.out.print("prgm1");
+     }  
+
+};
+
